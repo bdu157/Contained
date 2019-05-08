f551a76ffeb299132b77cdd79525b0025919b84a
Dongwoo Pae <dongwoopae@gmail.com>
2019-05-07 23:59:43 -0700

    set up the UI of toggleRoll and toggleZoom switches in Settings Screen

--- Contained/InfoViewController.swift
+++ Contained/InfoViewController.swift
@@ -16,6 +16,8 @@ class InfoViewController: UIViewController {
         
         
     }
+    
+    //Done button set up
     @IBAction func doneButtonTapped(_ sender: UIBarButtonItem) {
         navigationController?.popToRootViewController(animated: true)
     }
