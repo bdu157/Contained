807d40f8a21031bee0201ff5187cc41ca5ddc5aa
Dongwoo Pae <dongwoopae@gmail.com>
2019-05-08 00:34:52 -0700

    finished game screen - added import SpriteKit, Sprite Kit View and created skview property and added given codes- and created CustomScene.swift with given code. moved HappyCrab pictures into Assets.xcassets folder

--- Contained/SettingsViewController.swift
+++ Contained/SettingsViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Dongwoo Pae on 5/7/19.
 //  Copyright © 2019 Dongwoo Pae. All rights reserved.
 //
-
+import Foundation
 import UIKit
 
 class SettingsViewController: UIViewController {
