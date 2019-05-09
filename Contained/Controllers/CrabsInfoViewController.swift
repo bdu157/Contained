fd2cdeb662016091c093c0ce6b5d476e36741593
Dongwoo Pae <dongwoopae@gmail.com>
2019-05-08 17:57:31 -0700

    changed Done button to use unwindsegue as a practice unwindSegue

---
+++ Contained/Controllers/CrabsInfoViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CrabsInfoViewController.swift
+//  Contained
+//
+//  Created by Dongwoo Pae on 5/8/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import UIKit
+
+class CrabsInfoViewController: InfoViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToCrabsView(_ sender: UIStoryboardSegue) {}
+}
